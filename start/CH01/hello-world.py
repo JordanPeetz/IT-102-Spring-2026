33b3c05e0e296ef40cb8addd4d7c819dc50be762
JordanPeetz <peetz.jordan@student.greenriver.edu>
2026-04-18 05:20:09 +0000

    I created a hello world script

---
+++ start/CH01/hello-world.py
@@ -0,0 +1,13 @@
+"""
+This is just a script to prompt user name, then print hello and a positive affirmation
+"""
+
+#Prompt the user
+
+guest_name = input("What is your name: ")
+
+#This is to say hello
+print("Hello, ", guest_name)
+
+#Print Positive Affirmation
+print ("Whatever your mood is today, you should know you're absolutely killing it! ")
