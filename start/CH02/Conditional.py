afcc81ea430e1c953034aae94a8cd4642dd8c27e
JordanPeetz <peetz.jordan@student.greenriver.edu>
2026-04-25 01:39:26 +0000

    I added a few elif and else statements

--- start/CH02/Conditional.py
+++ start/CH02/Conditional.py
@@ -9,3 +9,7 @@ answer = input("Is today a good day? (y/n) ").lower()
 #This is an if statement checking if the string is equal to y and if so print yes it is
 if answer == "y":
     print("Yes it is")
+elif answer == "n":
+    print("Im sorry")
+else:
+    print("Please try again")
