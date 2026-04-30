b4420be0a4bc6c0fc90ac9fcd14f62abec5d9531
JordanPeetz <peetz.jordan@student.greenriver.edu>
2026-04-30 18:04:04 +0000

    Created a function for loop

--- start/CH02/Loop.py
+++ start/CH02/Loop.py
@@ -8,7 +8,6 @@ answer = input("Is today a good day? (y/n) ").lower()
 
 #This is an if statement checking if the string is equal to y and if so print yes it is
 if answer == "y":
-    for x in range(10):
         print("Yes it is")
 elif answer == "n":
     print("Im sorry")
